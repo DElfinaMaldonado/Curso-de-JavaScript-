e650322285ea16d18c672d73b38dc4ea0781fc2c
DElfinaMaldonado <del057677@gmail.com>
2025-04-21 08:40:11 -0600

    notas

--- 01-Tipos/02-TipodeDatos.js
+++ 01-Tipos/02-TipodeDatos.js
@@ -1,3 +1,4 @@
+// Primitivos → Cuando nosotros los vamos a buscar a la memoria RAM se encotrara el dato en el bloque que le fue asignado
 /* Primitivos
 -Number
 -String
@@ -5,6 +6,8 @@
 -Undefined
 -Null */
 
+
+// Cuando nosotros lo vayamos a consultar, vamos a tener que saltar a otro  de memoria 
 /* de referencia
 -Array
 -Object
