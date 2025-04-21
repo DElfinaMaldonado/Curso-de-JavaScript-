6f9c1e9c9b29779919479af45ffc0958f81c9f9f
DElfinaMaldonado <del057677@gmail.com>
2025-04-21 08:11:50 -0600

    cambbios

---
+++ 01-Tipos/01-Variables.js
@@ -0,0 +1,15 @@
+/* Si queremos declara variables, se utiliza let */
+let nombre ="Mundo";
+
+console.log(nombre);
+
+
+/* Se puede crear la variale pero no agregarle valor al mpomento que se crea*/
+let nombre1;
+
+console.log(nombre1); // Salidad: undefined → es cuando una variable ha sido definida pero no se le ha dado valor
+
+
+//Crear variables, debe empezar con texto o bien con guion bajo. NO CON NUMEROS
+let nombre2;
+let _nombre2;
