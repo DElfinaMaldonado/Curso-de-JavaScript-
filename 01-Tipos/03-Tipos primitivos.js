f649dcf2c2d18c11eb577b4d4a0f8cfa67be2f92
DElfinaMaldonado <del057677@gmail.com>
2025-04-21 08:58:42 -0600

    NOTAAS

---
+++ 01-Tipos/03-Tipos primitivos.js
@@ -0,0 +1,7 @@
+let numero = 1;
+let texto = "Hola Mundo";
+let verdadero = true;
+let falso = false;
+let noDefinido;
+let undef = undefined; // undefined → Puede ser un TIPO DE DATO o un VALOR
+let nulo = null; // es cuando queremos que la variable su valor es nulo → se utiliza → Cuando se tiene un menu con opciones pero el usuario no quiere seleccionar ninguna
---
+++ 01-Tipos/04-Constantes.js
@@ -0,0 +1,11 @@
+// Podemos cambiara el valor que tienen ascociadas las variables
+/*Todas las varaibles que nosotros declaramos pueden cambiar su valor en el futuro*/
+let apellido = "Montes";
+apellido = "Campos";
+console.log(apellido);
+
+/*CONST → Nos arroja un error si queremos cambiar el valor a la variable
+NO SE PUEDE CAMBIAR EL VALOR*/
+let apellido1 = "Montes";
+apellido1 = "Campos";
+console.log(apellido1);
