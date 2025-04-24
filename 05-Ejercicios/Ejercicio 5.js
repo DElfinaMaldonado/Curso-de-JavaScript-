db5ed05b9659c9c5374b8b4e661b9329b4ae6665
DElfinaMaldonado <del057677@gmail.com>
2025-04-24 09:27:42 -0600

    ejemplos y anotaciones

---
+++ 05-Ejercicios/Ejercicio 5.js
@@ -0,0 +1,15 @@
+/**
+ * Crear algorimo que devuelva numero
+ * menor y mayor del arrar
+ */
+
+// Salida → [-100, 55]
+
+let array = [2, 5, 7, 15, -5, -100, 55];
+
+function getMenorMayor(arr) {
+
+}
+
+let numeros = getMenorMayor(array)
+console.log(numeros);
