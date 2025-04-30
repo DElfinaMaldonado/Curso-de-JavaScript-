8ea44be1af9a4878c719077211ac7a175a69b791
DElfinaMaldonado <del057677@gmail.com>
2025-04-30 11:47:08 -0600

    ejercicios de logica

---
+++ 06-Restos de programacion/000-Resolucion.js
@@ -0,0 +1,38 @@
+/*- Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.*/
+
+/*JavaScript.com. */
+
+
+/*Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...)*/
+
+/* Esto se puede hacer en javacript para hacer comentarios en una sola linea */
+/**
+ * De esta manera
+ * se pueden hacer comentarios
+ * en varias lineas
+ */
+
+
+/*Crea una variable (y una constante si el lenguaje lo soporta). */
+
+let ejecicio02 = "practicas";
+const practica = 0;
+const practica1 = {};
+
+
+
+/* - Crea variables representando todos los tipos de datos primitivos
+ del lenguaje (cadenas de texto, enteros, booleanos...). */
+
+let nombre = "Delfina";
+let number = 4;
+let boolean = true;
+let undefin = undefined;
+let nulo = null;
+let decimal=1.13;
+
+
+
+/*  - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!" */
+let mensaje = "JavasScript";
+console.log("Hola " + mensaje);
