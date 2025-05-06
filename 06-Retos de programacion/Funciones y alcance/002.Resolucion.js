020097e70a76eea3cc2c28afece8022246eba5ed
DElfinaMaldonado <del057677@gmail.com>
2025-05-06 16:21:16 -0600

    ejercicios

--- 06-Retos de programacion/Funciones y alcance/002.Resolucion.js
+++ 06-Retos de programacion/Funciones y alcance/002.Resolucion.js
@@ -83,6 +83,8 @@ function cumplir() {
     let resultado = cumplir();
     console.log(resultado);
 
+
+
 /** Problema 1:
  *Escribe una función que imprima "¡Bienvenido al sistema!" en la consola
 cada vez que sea llamada.
@@ -101,12 +103,82 @@ mensaje();
 mostrando cada número en orden descendente.
  */
 let cuenta = 10;
-function regresiva (){
-    while (cuenta <= 10){
-        if (cuenta == 1) {
-            console.log('Cuenta regresiva:', cuenta)
+function regresiva() {
+    while (cuenta >= 0){
+            console.log('Esto es un cuenta regresiva', cuenta);
+            cuenta--;
         }
-        cuenta--;
+    }
+regresiva();
+
+
+/** Problema 3:
+ * Escribe una función que genere una tabla de multiplicar del 1 al 10
+ * para un número aleatorio entre 1 y 10, mostrando cada resultado en la consola.
+*/
+
+function tablaMultiplicar() {
+    const numeroAleatorio= Math.floor(Math.random() * 100) + 1;
+    console.log("Este es un numero aleatorio:", numeroAleatorio);
+    for (let numero3 = 1; numero3  <= 10; numero3 ++) {
+        const resultado = numero3 * numeroAleatorio;
+        console.log(`${numero3} * ${numeroAleatorio} = ${resultado}`);
+}
+}
+tablaMultiplicar();
+
+
+
+
+
+                /** FUNCIONES CON UN PARAMETRO
+ * Devuelve undefined. Si se requiere que la funcion no devuelva nada, no se utiliza retur.*/
+
+function hello(nombre) {
+    console.log("Hola, " + nombre + "!");
+}
+    hello("Frida");
+
+
+/** PROBLEMA 1:
+ *Define una función que reciba un número como parámetro y determine si el número e
+ s positivo, negativo o cero. La función debe imprimir un mensaje indicando el tipo
+ de número que se ha recibido.
+ */
+
+function parametros(numero){
+    if (numero > 0) {
+        console.log('positivo');
+    } else if (numero < 0) {
+        console.log('negtivo')
+    }else {
+        console.log('cero');
     }
 }
-regresiva();
+parametros(4);
+parametros(-1);
+parametros(0);
+
+
+
+/** PROBLEMA 2:
+ * Define una función que reciba un número entero como parámetro y
+ * muestre en la consola todos los números pares desde 1 hasta ese número.
+ */
+
+function pares(entero){
+    while(entero <=10){
+        entero++;
+        if(entero)
+    }
+}
+
+let impresion = 0;
+while (impresion <= 10) {
+    impresion++;
+    if (impresion % 2 === 0 ) {
+        continue;
+}
+console.log('omitir numeros pares ' + impresion);
+}
+
