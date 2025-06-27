3f933b33e58bec3e5da1bd68d1fa6f347e7e1fe4
DElfinaMaldonado <del057677@gmail.com>
2025-06-27 13:45:58 -0600

    ejercicios

--- 06-Retos de programacion/006 Recursividad/006.Resolucion.js
+++ 06-Retos de programacion/006 Recursividad/006.Resolucion.js
@@ -8,6 +8,10 @@ let recursividad_cuenta = (numero) => {
 };
 console.log(recursividad_cuenta(5));
 
+
+
+// EJERCICIOS FACILES
+
 // // Ejercicio 1: Suma de números consecutivos:
 // /*Escribe una función recursiva que reciba un número entero positivo y devuelva la suma de todos los enteros desde 1 hasta ese número. */
 function numeros_Consecutivos() {
@@ -71,7 +75,7 @@ console.log(invertirCadena('Hola'));
 // EJERCICIO 5: Número de elementos en un arreglo anidado
 /*Implementa una función que cuente cuántos elementos hay dentro de un arreglo que puede contener otros arreglos, usando recursión para recorrer todos los niveles.  */
 
-function arreglo_Anidado () {
+function arreglo_Anidado() {
     const arreglo_one = [
         [1, 2, 3],
         [4, 5, 6],
@@ -88,3 +92,125 @@ function arreglo_Anidado () {
 }
 arreglo_Anidado();
 
+
+
+
+// EJERCICIO INTERMEDIOS
+
+// EJERCICIO 1: Suma recursiva de un arreglo
+/*Dado un arreglo de números, implementa una función recursiva que calcule la suma de todos sus elementos */
+function sumaArreglo(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+    return arr[0] + sumaArreglo(arr.slice(1));
+}
+const numeros = [10, 20, 30, 40, 50, 60, 70, 80];
+console.log(sumaArreglo(numeros));
+
+//EJERCICIO 2: Convertir número decimal a binario
+/*  Crea una función recursiva que reciba un número decimal y devuelva su representación binaria como string. */
+function numero_Decimal_Binario() {
+    let conversion_DecimalBinario = (
+        numero) => {
+        if (numero === 0) {
+            return "";
+        }
+        return conversion_DecimalBinario(Math.floor(numero / 2)) + (numero % 2);
+    };
+    console.log(conversion_DecimalBinario(10));
+}
+numero_Decimal_Binario();
+// ↑
+//  1111111
+//  8421
+//  1010
+
+// EJERCICIO 3: Contar digitos
+/* Escribe una funcion recursiva que cuente cuantos digitos tiene un numero entero positivo */
+// function contador_Digitos (coste) {
+// const digitos = 0;
+// for (let i = coste.length - 1; i >= 0; i--) {
+//         digitos += 1;
+//     }
+//     return digitos;
+// }
+// coste = prompt("Ingrese el numero para calcular sus digitos");
+// producto = contador_Digitos (coste);
+// console.log("Ingrese el digito", producto);
+
+// EJERCICIO 4: Buscar un elemento en un arreglo
+/* Dado un arreglo desordenado, implementa una funcion recursiva y devuelva su representacion binaria como string*/
+function buscarElemento(array, elemento, indice = 0) {
+    if (indice >= array.length) {
+        return -1;
+    }
+    if (array[indice] === elemento) {
+        return indice;
+    }
+    return buscarElemento(array, elemento, indice + 1);
+}
+buscarElemento(arry = [1, 2, 3, 4, 5], elemento = 5);
+
+const Arreglo_Original = [1, 2, 3, 4, 5];
+const copia_Arreglo = [...Arreglo_Original];
+console.log(copia_Arreglo);
+
+// EJERCICIO 5:
+/* */
+function InvertirCadena(enlace) {
+    let actualEnlace = "";
+    for (let i = enlace.length - 1; i >= 0; i--) {
+        actualEnlace += enlace[i];
+    }
+    return actualEnlace;
+}
+console.log(InvertirCadena('Adios'));
+
+
+//  * DIFICULTAD EXTRA (opcional):
+//  * Utiliza el concepto de recursividad para:
+//  * - Calcular el factorial de un número concreto (la función recibe ese número).
+//  * - Calcular el valor de un elemento concreto (según su posición) en la
+//  *   sucesión de Fibonacci (la función recibe la posición).
+//  */
+
+// 1. Calcular el factorial de un numero concreto (La funcion recibe ese numero).
+function factorial_Contar(clave) {
+    if (clave < 0)
+        return -1;
+    else if (clave == 0)
+        return 1;
+    else {
+        return (clave * factorial_Contar(clave - 1));
+    }
+}
+console.log(factorial_Contar(10));
+// 10 * 9 = 90 90 * 8 = 720 720 * 7 = 5040 5040 * 6 = 30240 30240 * 5 = 151200 151200 * 4 = 604800 604800 * 3 = 1814400 1814400 * 2 = 3628800
+
+// Calcular  el valor  de un elemento crocretos (según su posición) en la  sucesión de Fibonaccion
+let factorial = (importe) => {
+    if (importe < 0) {
+        console.loglog(" Los numeros negativos no sonvalidos");
+    } else if (importe <= 0) {
+        console.log("Los numeros negativos no son validos");
+        return 0;
+    } else if (importe == 1) {
+        return 0;
+    } else if (importe == 2) {
+        return 1;
+    } else {
+        (importe - 1) + (importe - 2);
+    }
+}
+console.log("El factorial de 5 es:", factorial(5));
+
+
+
+// EJERCICIOS DIFICILES
+// EJERCICIO 1: Resolver un laberinto matriz 2D.
+/* Implementar una funcion que encuentre una salida desde un punto inicial hasta una celda objetivo en una matriz representando un laberinto con obstaculos.*/
+function  laberinto(laberinto, fila, columna, salida) {
+    if (fila < 0 || fila >= laberinto.length || columna < 0 || columna >= laberinto[0].length || laberinto[fila][columna]
+
+}
